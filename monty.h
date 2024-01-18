52c9b0f6bd9c1bd0e77b5be022e799f72255183f
Thato Mongwe <thatomongwe47@gmail.com>
2024-01-18 14:21:10 +0000

    Update header file

--- monty.h
+++ monty.h
@@ -4,16 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define STACK_SIZE 1000
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
 
 typedef struct {
-    int array[STACK_SIZE];
-    int top;
+    Node *top;
 } Stack;
 
 void initializeStack(Stack *stack);
 int isEmpty(Stack *stack);
-int isFull(Stack *stack);
 void push(Stack *stack, int value, int line_number);
 void pall(Stack *stack);
 
