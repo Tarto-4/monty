9b0f32703a570405cd7288a477a7666463e2ab0e
Thato Mongwe <thatomongwe47@gmail.com>
2024-01-18 15:23:45 +0000

    Code maintainability

--- push.c
+++ push.c
@@ -1,5 +1,13 @@
 #include "monty.h"
 
+/**
+* f_push - function that adds node to the stack
+* @stack: double head pointer to the stack
+* @line_number: line count
+* @value: intt value
+* Return: nothing
+*/
+
 void push(Stack *stack, int value, int line_number)
 {
     if (stack->top == STACK_SIZE - 1)
